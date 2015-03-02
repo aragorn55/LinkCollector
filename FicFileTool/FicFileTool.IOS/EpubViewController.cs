ffe0bde1efd6b639ccd1ef8eedcabc4126a88e4d
aragorn55 <student@students-mac-37.fvtc.edu>
2015-03-01 20:47:06 -0600

    iOS

---
+++ FicFileTool/FicFileTool.IOS/EpubViewController.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace FicFileTool.IOS
+{
+	public partial class EpubViewController : UIViewController
+	{
+		public EpubViewController () : base ("EpubViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
